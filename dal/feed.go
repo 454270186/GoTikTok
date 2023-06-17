fede5f58e9884c012e5df765c503e993a0577899
454270186 <sc21ey@leeds.ac.uk>
2023-06-17 11:43:25 +0800

    add feed dal interface:

---
+++ dal/feed.go
@@ -0,0 +1,34 @@
+package dal
+
+import (
+	"context"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type FeedDB struct {
+	DB *gorm.DB
+}
+
+func NewFeedDB() FeedDB {
+	return FeedDB{
+		DB: newDB(),
+	}
+}
+
+func (f FeedDB) GetVideoLists(ctx context.Context, limit int, latestTime int64) ([]*Video, error) {
+	videos := make([]*Video, 0)
+
+	if latestTime == 0 {
+		curTime := time.Now().UnixMilli()
+		latestTime = curTime
+	}
+
+	err := f.DB.WithContext(ctx).Limit(limit).Find(&videos, "update_at < ?", time.UnixMilli(latestTime)).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return videos, nil
+}
