81aaadce91e4d6171dac05ea141b9dc54183b440
454270186 <sc21ey@leeds.ac.uk>
2023-06-17 11:48:21 +0800

    add feed rpc client

--- cmd/rpccli/feed.go
+++ cmd/rpccli/feed.go
@@ -1,1 +1,19 @@
 package rpccli
+
+import (
+	"github.com/454270186/GoTikTok/rpc/feed/feedservice"
+	"github.com/zeromicro/go-zero/core/discov"
+	"github.com/zeromicro/go-zero/zrpc"
+)
+
+func NewFeedCli() feedservice.FeedService {
+	conn := zrpc.MustNewClient(zrpc.RpcClientConf{
+		Etcd: discov.EtcdConf{
+			Hosts: []string{"127.0.0.1:2379"},
+			Key: "feeed.rpc",
+		},
+	})
+
+	feedCli := feedservice.NewFeedService(conn)
+	return feedCli
+}
