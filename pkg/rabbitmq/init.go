f2f732e3f245b571ea101b5492b853ad72cd979c
454270186 <sc21ey@leeds.ac.uk>
2023-07-15 15:10:51 +0800

    add rabbitmq interface

--- pkg/rabbitmq/init.go
+++ pkg/rabbitmq/init.go
@@ -1,2 +1,28 @@
 package rabbitmq
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	mqAddr string
+	queName string
+)
+
+func init() {
+	mqMap, err := godotenv.Read()
+	if err != nil {
+		panic(err)
+	}
+
+	mqAddr = mqMap["MQ_ADDR"]
+	queName = mqMap["MQ_QUE_NAME"]
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s\n", msg, err.Error())
+	}
+}
--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -1,13 +1,92 @@
 package rabbitmq
 
 import (
+	"context"
+	"errors"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+// User direct exchange (default)
+// Every RabbitMQ instance has unique queue name
 type RabbitMQ struct {
 	conn *amqp.Connection
 	channel *amqp.Channel
 	QueueName string
-	
+}
 
+// Create a RabbitMQ instance in Simple mode
+func NewSimple() *RabbitMQ {
+	rabbitMQ := &RabbitMQ{}
+	var err error
+	rabbitMQ.conn, err = amqp.Dial(mqAddr)
+	failOnError(err, "Fail to connect to RabbitMQ")
+
+	rabbitMQ.channel, err = rabbitMQ.conn.Channel()
+	failOnError(err, "Fail to open a channel")
+
+	rabbitMQ.QueueName = queName
+
+	return rabbitMQ
+}
+
+// Publish msg to default exchange using queue name for routing
+func (r *RabbitMQ) PubWithCtx(ctx context.Context, body []byte) error {
+	_, err := r.channel.QueueDeclare(
+		r.QueueName,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return errors.New("fail to declare queue: " + err.Error())
+	}
+
+	err = r.channel.PublishWithContext(ctx, 
+		"", // user default exchange
+		r.QueueName,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body: body,
+		},
+	)
+	if err != nil {
+		return errors.New("fail to publish a msg: " + err.Error())
+	}
+
+	return nil
+}
+
+//  Get msgs from default exchange using queue name for routing
+func (r *RabbitMQ) GetMsgs() (<- chan amqp.Delivery, error) {
+	_, err := r.channel.QueueDeclare(
+		r.QueueName,
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, errors.New("fail to declare queue: " + err.Error())
+	}
+
+	msgs, err := r.channel.Consume(
+		r.QueueName,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, errors.New("fail to get msgs from queue: " + err.Error())
+	}
+
+	return msgs, nil
 }
