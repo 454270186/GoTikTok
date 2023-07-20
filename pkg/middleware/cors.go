549f9f4a179e0fde1b54e85cbea4c4dc63a6fbfb
454270186 <sc21ey@leeds.ac.uk>
2023-07-20 15:56:28 +0800

    update cors middleware comment

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func Cors() gin.HandlerFunc {
 	return func (c *gin.Context)  {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // "*" can be replaced with specific domain
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
