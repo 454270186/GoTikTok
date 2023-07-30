27a9f0c64f55ef52137ccd43af96855ea5dd1e59
454270186 <sc21ey@leeds.ac.uk>
2023-07-30 13:58:24 +0800

    finish jaeger opentracing api

--- pkg/tracing/tracer.go
+++ pkg/tracing/tracer.go
@@ -1,7 +1,9 @@
 package tracing
 
 import (
+	"errors"
 	"io"
+	"log"
 
 	"github.com/opentracing/opentracing-go"
 	jaegerConfig "github.com/uber/jaeger-client-go/config"
@@ -39,14 +41,40 @@ func InitTracer(serviceName, agentHostPort string) error {
 	return nil
 }
 
+// Return parent span(or a new span if no parent) and error
 func getParentSpan(operationName, traceID string, isRoot bool) (opentracing.Span, error) {
-	// TODO
-	return nil, nil
+	if Tracer == nil {
+		log.Println("Tracer is nil")
+		return nil, errors.New("tracer is nil")
+	}
+
+	parentSpanCtx, err := Tracer.Extract(opentracing.TextMap, opentracing.TextMapCarrier{"UBER-TRACE-ID": traceID})
+	if err != nil {
+		if isRoot {
+			return Tracer.StartSpan(operationName), nil
+		}
+		log.Println("error while extract: ", err)
+		return nil, err
+	}
+
+	return Tracer.StartSpan(operationName, opentracing.ChildOf(parentSpanCtx)), nil
 }
 
 func StartSpan(operationName, parentTracID string, isRoot bool) (opentracing.Span, string, error) {
-	// TODO
-	return nil, "", nil
+	parentSpan, err := getParentSpan(operationName, parentTracID, isRoot)
+	if err != nil {
+		log.Println("no span return: ", err)
+		return nil, "", errors.New("error while get parent span")
+	}
+
+	carrier := opentracing.TextMapCarrier{}
+	err = Tracer.Inject(parentSpan.Context(), opentracing.TextMap, carrier)
+	if err != nil {
+		return nil, "", err
+	}
+
+
+	return parentSpan, carrier["uber-trace-id"], nil
 }
 
 func FinishSpan(span opentracing.Span) {
